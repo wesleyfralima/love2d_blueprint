d2546de5abb9c7a195a5054010c6383a9b6c7658
wesleyfralima <wesleyfralima@gmail.com>
2023-06-03 12:52:03 -0300

    Made player animations faster

--- src/entity/entity_defs.lua
+++ src/entity/entity_defs.lua
@@ -4,51 +4,51 @@ ENTITY_DEFS = {
             ['idle-right'] = {
                 framesCol = '1-5',
                 framesRow = 1,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_idle'
             },
             ['idle-left'] = {
                 framesCol = '1-5',
                 framesRow = 2,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_idle'
             },
             ['walk-right'] = {
                 framesCol = '1-6',
                 framesRow = 1,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_walk'
             },
             ['walk-left'] = {
                 framesCol = '1-6',
                 framesRow = 2,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_walk'
             },
             ['jump-right'] = {
                 framesCol = '1-3',
                 framesRow = 1,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_jump',
                 onloop = 'pauseAtEnd'
             },
             ['jump-left'] = {
                 framesCol = '1-3',
                 framesRow = 2,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_jump',
                 onloop = 'pauseAtEnd'
             },
             ['fall-right'] = {
                 framesCol = 1,
                 framesRow = 1,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_fall'
             },
             ['fall-left'] = {
                 framesCol = 1,
                 framesRow = 2,
-                interval = 0.15,
+                interval = 0.1,
                 texture = 'player_fall'
             },
         }
